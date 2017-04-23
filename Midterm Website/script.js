126b6121e064aa00f65bbaf61c07c3351e976a17
Christian Lalas <2143836@slu.edu.ph>
2017-04-23 17:16:45 +0800

    edited js

--- Midterm Website/script.js
+++ Midterm Website/script.js
@@ -1,11 +1,30 @@
-$(function(){
-	$('#htmlSection').click(function(){
-		$('#htmlPanel').slideToggle(300);
-	})
-})
 
 $(function(){
 	$('#cssSection').click(function(){
 		$('#cssPanel').slideToggle(300);
+	})
+})
+
+$(function(){
+	$('#jsSection').click(function(){
+		$('#jsPanel').slideToggle(300);
+	})
+})
+
+$(function(){
+	$('#section1').click(function(){
+		$('#section1a').slideToggle(300);
+	})
+})
+
+$(function(){
+	$('#section2').click(function(){
+		$('#section2a').slideToggle(300);
+	})
+})
+
+$(function(){
+	$('#section3').click(function(){
+		$('#section3a').slideToggle(300);
 	})
 })
