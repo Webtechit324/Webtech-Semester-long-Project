d798a7f7d9a6298ece1ca41d461fe8d7a532abcf
Christian Lalas <2143836@slu.edu.ph>
2017-04-23 14:35:12 +0800

    Added java script

---
+++ Midterm Website/script.js
@@ -0,0 +1,11 @@
+$(function(){
+	$('#htmlSection').click(function(){
+		$('#htmlPanel').slideToggle(300);
+	})
+})
+
+$(function(){
+	$('#cssSection').click(function(){
+		$('#cssPanel').slideToggle(300);
+	})
+})
